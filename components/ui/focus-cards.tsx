e9ccce087f76ed90a80f36041345609790f7a7d6
Anish202020 <anishkumarbdmi@gmail.com>
2024-10-02 18:15:33 +0530

    commit changes

--- components/ui/focus-cards.tsx
+++ components/ui/focus-cards.tsx
@@ -10,7 +10,7 @@ export const Card = React.memo(
     hovered,
     setHovered,
   }: {
-    card: any;
+    card: any | string;
     index: number;
     hovered: number | null;
     setHovered: React.Dispatch<React.SetStateAction<number | null>>;
